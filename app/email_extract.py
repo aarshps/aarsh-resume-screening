f7e595a5fcb78ed0d7a6f74ab8d200696cf1b5bf
Aarsh <aarshps@gmail.com>
2023-06-18 15:30:01 +0530

    Update extract_email to extract multiple emails

--- app/email_extract.py
+++ app/email_extract.py
@@ -3,14 +3,11 @@ import re
 def extract_email(email_content):
     pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b'
     email_pattern_compile = re.compile(pattern)
-    email_extracted_result = email_pattern_compile.search(email_content)
-    email_extracted = email_extracted_result.group()
+    emails_extracted = email_pattern_compile.findall(email_content)
 
-    print(email_extracted_result)
-
-    return email_extracted
+    return emails_extracted
 
 email_content = "hi what are you doing? aarshps@gmail.com has all the time! aarshps2@gmail.com"
-email_extracted = extract_email(email_content)
+emails_extracted = extract_email(email_content)
 
-print(email_extracted)
+print(emails_extracted)
